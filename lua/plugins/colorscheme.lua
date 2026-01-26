e4368b33a637678a52879e257e4503c9de02c62c
Nuggyyy <121103879+Nuggyyy@users.noreply.github.com>
2026-01-26 12:55:13 +0100

    added iceberg colorscheme. if you want to change back, change the colorscheme variable and delete the comments for the two indentation colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,14 +6,25 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd([[colorscheme pink-as-fox]])
-
       -- Fix indent line colors
       -- vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#502542" })
-      vim.api.nvim_set_hl(0, "SnacksIndentScope", { fg = "#CC8FB7" })
+
+      -- vim.api.nvim_set_hl(0, "SnacksIndentScope", { fg = "#CC8FB7" })
 
       -- Make NonText equal to dark purple so that things are not hidden due to the pink-as-fox background color mismatch
-      vim.api.nvim_set_hl(0, "NonText", { fg = "#311628" })
+
+      -- vim.api.nvim_set_hl(0, "NonText", { fg = "#311628" })
     end,
+  },
+  {
+    "cocopon/iceberg.vim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "iceberg",
+    },
   },
 }
